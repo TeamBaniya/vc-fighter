59ef3f5543f1e48a692f954781e842caae710d4a
ꜱᴘᴧʀꜱʜ ʙᴧɴɪʏᴧ <89186991+TeamBaniya@users.noreply.github.com>
2026-04-26 17:18:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # Get these from https://my.telegram.org
 API_ID = 24168862  # Your API ID
-API_HASH = "your_api_hash_here"  # Your API Hash
+API_HASH = "916a9424dd1e58ab7955001ccc0172b3"  # Your API Hash
 
 # Bot token from @BotFather
-BOT_TOKEN = "your_bot_token_here"  # Your bot token
+BOT_TOKEN = "8663096796:AAHHgqmkE2KlHmETBgApYu6dR43bxwEifbU"  # Your bot token
 
 # Your Telegram user ID (owner)
 OWNER_ID = 8593970102  # Your Telegram user ID
