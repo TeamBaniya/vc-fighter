d2108e45f5f93ab180ac349f765158a25ba51ee4
ꜱᴘᴧʀꜱʜ ʙᴧɴɪʏᴧ <89186991+TeamBaniya@users.noreply.github.com>
2026-05-12 15:25:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ API_ID = 24168862  # Your API ID
 API_HASH = "916a9424dd1e58ab7955001ccc0172b3"  # Your API Hash
 
 # Bot token from @BotFather
-BOT_TOKEN = "8663096796:AAHHgqmkE2KlHmETBgApYu6dR43bxwEifbU"  # Your bot token
+BOT_TOKEN = "8620719005:AAHesi6NTqNFt1IoXWbE1p11-uzDhTYvejI"  # Your bot token
 
 # Your Telegram user ID (owner)
-OWNER_ID = 8593970102  # Your Telegram user ID
+OWNER_ID = 7035596015  # Your Telegram user ID
 
 # Optional: Default account session string (for "Default Account" feature)
 # Get this using @StringSessionBot
