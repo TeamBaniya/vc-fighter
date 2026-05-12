19f27813f3c74faf7b88cdb97d51ad0f29d61f3d
ꜱᴘᴧʀꜱʜ ʙᴧɴɪʏᴧ <89186991+TeamBaniya@users.noreply.github.com>
2026-05-12 15:51:50 +0530

    Update test_pytgcalls.py

--- test_pytgcalls.py
+++ test_pytgcalls.py
@@ -5,14 +5,15 @@ from pytgcalls import GroupCallFactory
 
 async def main():
     # Create a dummy client for testing
-    client = Client("test_session")
+    client = Client("test_session", in_memory=True)
+    await client.start()
     
     # Get group call instance
     group_call = GroupCallFactory(client).get_group_call()
     
     # Print available methods
     methods = [x for x in dir(group_call) if not x.startswith('_')]
-    print("Available methods:", methods)
+    print("Available methods:", methods[:20])  # Print first 20 methods
     
     await client.stop()
 
